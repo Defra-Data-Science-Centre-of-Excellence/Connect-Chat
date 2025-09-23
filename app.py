6d3271881d0ea9308aa852b0189ce20589dd9a63
Fraser Buchanan <135606706+Fraser-Buchanan@users.noreply.github.com>
2025-09-23 14:41:20 +0100

    Update app.py
    adding audience GUID for api integration to with_user_session_token

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def server(input: Inputs, output: Outputs, session: Session):
         if user_session_token:
             try:
                 print('before client call')
-                client = client.with_user_session_token(user_session_token)
+                client = client.with_user_session_token(user_session_token, audience="d4bd6e9b-bed3-475b-a773-d1848e14df69")
                 print('Client worked!')
             except ClientError as err:
                 print('was an error after all')
